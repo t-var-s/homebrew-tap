05a2e20cc2a9929c5b2553868d0e8b45c397fa5f
Ricardo Tavares <ricardo.tavares@viewfromtheweb.com>
2023-05-21 18:59:10 +0100

    update SHA256 sum

--- Formula/wpskim.rb
+++ Formula/wpskim.rb
@@ -2,7 +2,7 @@ class Wpskim < Formula
   desc "A CLI tool to retrieve external links, e-mails and documents from WordPress sites"
   homepage "https://github.com/t-var-s/wpskim"
   url "https://github.com/t-var-s/wpskim/releases/download/v0.2.0/wpskim-0.2.0-apple-arm64.tar.gz"
-  sha256 "e287519fc1dd9517c35cecf9541a9bb231c86961"
+  sha256 "d922c53cf06367c8bd3b9463e2d37e6f0e66dfb96546251b025d10c908f1baf3"
   version "0.2.0"
 
   def install
