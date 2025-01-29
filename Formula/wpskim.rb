14c7f86b2edcbe1f0522e9cb294cb4d44052f2e5
Ricardo Tavares <6660327+t-var-s@users.noreply.github.com>
2025-01-29 20:15:55 +0000

    Update wpskim.rb

--- Formula/wpskim.rb
+++ Formula/wpskim.rb
@@ -1,9 +1,9 @@
 class Wpskim < Formula
   desc "A CLI tool to retrieve external links, e-mails and documents from WordPress sites"
   homepage "https://github.com/t-var-s/wpskim"
-  url "https://github.com/t-var-s/wpskim/releases/download/v0.2.0/wpskim-0.2.0-apple-arm64.tar.gz"
-  sha256 "d922c53cf06367c8bd3b9463e2d37e6f0e66dfb96546251b025d10c908f1baf3"
-  version "0.2.0"
+  url "https://github.com/t-var-s/wpskim/releases/download/v0.3.0/wpskim-0.3.0-apple-arm64.tar.gz"
+  sha256 "78a97448cbb4aacb0eae5e7503a06018650c3e7441e3d6a91a93e90303772bde"
+  version "0.3.0"
 
   def install
     bin.install "wpskim"
